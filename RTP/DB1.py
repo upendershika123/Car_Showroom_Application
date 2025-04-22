2cb9977d3bf2f91893e11fb9a6cb4a68334b8642
upendershika123 <supender_cse220543@mgit.ac.in>
2025-04-22 10:24:55 +0530

    Initial commit

--- RTP/DB1.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-mydb=mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="")
-cur=mydb.cursor()
-cur.execute("CREATE DATABASE DB1")
-print("database created")
